7470ef84dcc2cda1a61475928abaa9a403f5bf0f
erenkamer1 <erenkamer28@gmail.com>
2025-10-31 12:41:17 +0300

    tests are added

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!((@)?react-native|@reduxjs|react-redux|immer)/)',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|svg|webp)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 };
